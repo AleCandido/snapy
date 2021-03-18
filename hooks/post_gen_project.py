8bbe77a2ddf6d2e6b065d77840bfd124762c3d3b
Alessandro Candido <candido.ale@gmail.com>
2021-03-18 20:25:52 +0100

    Init git repo during scaffolding

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,7 +6,13 @@ def install(package, *options, venv=False):
     if venv:
         options = " ".join(options)
         subprocess.check_call(
-            f". env/bin/activate && python -m pip install {package} {options} && deactivate",
+            " && ".join(
+                [
+                    ". env/bin/activate",
+                    f"python -m pip install {package} {options}",
+                    "deactivate",
+                ]
+            ),
             shell=True,
         )
     else:
@@ -21,14 +27,48 @@ def create_environment():
     virtualenv.cli_run(["env", "--system-site-packages"])
 
 
+def create_repo():
+    import pygit2
+
+    # create first commit
+    repo = pygit2.init_repository(".")
+    repo.index.add_all()
+    user = repo.default_signature
+    tree = repo.index.write_tree()
+    parent = []
+    commit = repo.create_commit(
+        "HEAD", user, user, "Init {{ cookiecutter.project_slug }}", tree, parent
+    )
+    repo.index.write()
+
+    # create first tag
+    repo.create_tag(
+        "v0.0.1",
+        commit.hex,
+        pygit2.GIT_OBJ_COMMIT,
+        repo[commit].author,
+        repo[commit].message,
+    )
+
+
 def create_docs():
     subprocess.check_call(
-        ["sphinx-quickstart", "-q", "-p", "my", "-a", "me", "--sep"], cwd="docs"
+        " && ".join(
+            [
+                ". ../env/bin/activate",
+                "sphinx-quickstart --quiet --project {{ cookiecutter.project_slug }} --author '{{ cookiecutter.full_name }}' --sep",
+                "deactivate",
+            ]
+        ),
+        cwd="docs",
+        shell=True,
     )
 
 
 def run_tests():
-    subprocess.check_call(["pytest"])
+    subprocess.check_call(
+        " && ".join([". env/bin/activate", "pytest", "deactivate"]), shell=True
+    )
 
 
 if __name__ == "__main__":
@@ -36,10 +76,12 @@ if __name__ == "__main__":
 
     create_environment()
 
-    #  install("--editable", ".", venv=True)
+    install("pygit2", "--upgrade", venv=True)
+    create_repo()
+    install("--editable", ".", venv=True)
 
     install("--requirement", "doc_requirements.txt", "--ignore-installed", venv=True)
     create_docs()
 
     install("--requirement", "test_requirements.txt", "--ignore-installed", venv=True)
-    #  run_tests()
+    run_tests()
